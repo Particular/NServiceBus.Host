3af780cc1f19b49040ea80b296d959ac0eba8ee8
indualagarsamy <indu.alagarsamy@gmail.com>
2014-10-24 11:39:59 -0700

    Added a better comment regarding the selection of Persistence,Fixes #27 , Also removed the AsA_Server as its no longer needed. Fixes #1

--- packaging/nuget/nservicebus.host/EndpointConfig.cs
+++ packaging/nuget/nservicebus.host/EndpointConfig.cs
@@ -7,10 +7,15 @@ namespace rootnamespace
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    public class EndpointConfig : IConfigureThisEndpoint
     {
         public void Customize(BusConfiguration configuration)
         {
+            // For production use, please select a durable persistence.
+            // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
+            // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
+
+            // For development/debugging you can also use, configuration.UsePersistence<InMemoryPersistence>();
             configuration.UsePersistence<PLEASE_SELECT_ONE>();
         }
     }
