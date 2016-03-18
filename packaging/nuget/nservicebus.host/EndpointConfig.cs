421d0d91719f8033ec8770282176da6715652121
Simon Cropp <simon.cropp@gmail.com>
2016-03-18 18:12:27 +1100

    better config template

--- packaging/nuget/nservicebus.host/EndpointConfig.cs
+++ packaging/nuget/nservicebus.host/EndpointConfig.cs
@@ -5,11 +5,11 @@ namespace rootnamespace
 
     public class EndpointConfig : IConfigureThisEndpoint
     {
-        public void Customize(BusConfiguration configuration)
+        public void Customize(EndpointConfiguration endpointConfiguration)
         {
-            // NServiceBus provides multiple durable storage options please refer to our documentation for more details.
-            // If you don't need a durable storage you can also use, configuration.UsePersistence<InMemoryPersistence>();
-            configuration.UsePersistence<PLEASE_SELECT_ONE>();
+            // NServiceBus provides multiple durable storage options, including SQL Server, RavenDB, and Azure Storage Persistence. 
+            // Refer to the documentation for each option for more details.
+            endpointConfiguration.UsePersistence<PLEASE_SELECT_ONE>();
         }
     }
 }
