628108dce84c24027b9c9757d494676342dde44b
Simon Cropp <simon.cropp@gmail.com>
2016-03-18 18:13:13 +1100

    typo

--- packaging/nuget/nservicebus.host/EndpointConfig.cs
+++ packaging/nuget/nservicebus.host/EndpointConfig.cs
@@ -8,7 +8,7 @@ namespace rootnamespace
         public void Customize(EndpointConfiguration endpointConfiguration)
         {
             // NServiceBus provides multiple durable storage options, including SQL Server, RavenDB, and Azure Storage Persistence. 
-            // Refer to the documentation for each option for more details.
+            // Refer to the documentation for more details on specific options.
             endpointConfiguration.UsePersistence<PLEASE_SELECT_ONE>();
         }
     }
