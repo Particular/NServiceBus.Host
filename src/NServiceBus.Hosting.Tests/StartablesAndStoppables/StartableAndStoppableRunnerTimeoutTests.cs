62eef61afea47a48e553717f55e9a735c705e977
Colin Higgins <colin-higgins@users.noreply.github.com>
2016-04-04 11:16:32 -0400

    IWantToRunWhenWhenEndpointStartsAndStop Blocking Warning
    * Add timeout with warning message
    
    * Refactor LongRunningWarning and add tests
    
    * Add tests for quick running with no warning and refactor warning method

---
+++ src/NServiceBus.Hosting.Tests/StartablesAndStoppables/StartableAndStoppableRunnerTimeoutTests.cs
@@ -0,0 +1,91 @@
+﻿namespace NServiceBus.Hosting.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Logging;
+    using Moq;
+    using NUnit.Framework;
+
+    public class StartableAndStoppableRunnerTimeoutTests
+    {
+        static TimeSpan TimeToWarn = TimeSpan.FromSeconds(3);
+        static TimeSpan LongRunningTimespan = TimeToWarn.Add(TimeSpan.FromSeconds(2));
+        static TimeSpan ShortRunningTimespan = TimeToWarn.Subtract(TimeSpan.FromSeconds(2));
+        const string startupWarning = ".+The endpoint will not start until this operation has completed.+";
+        const string shutdownWarning = ".+The endpoint will not shut down.+";
+
+        [Test]
+        public async Task Should_log_warning_on_long_start()
+        {
+            var mockLogger = await RunStartableWarningTest(new LongRunningStartable());
+
+            mockLogger.Verify(m => m.Warn(It.IsRegex(startupWarning)));
+        }
+
+        [Test]
+        public async Task Should_log_warning_on_long_stop()
+        {
+            var mockLogger = await RunStartableWarningTest(new LongRunningStartable());
+
+            mockLogger.Verify(m => m.Warn(It.IsRegex(shutdownWarning)));
+        }
+        [Test]
+        public async Task Should_not_log_warning_on_quick_start()
+        {
+            var mockLogger = await RunStartableWarningTest(new QuickRunningStartable());
+
+            mockLogger.Verify(m => m.Warn(It.IsRegex(startupWarning)), Times.Never);
+        }
+
+        [Test]
+        public async Task Should_not_log_warning_on_quick_stop()
+        {
+            var mockLogger = await RunStartableWarningTest(new QuickRunningStartable());
+
+            mockLogger.Verify(m => m.Warn(It.IsRegex(shutdownWarning)), Times.Never);
+        }
+
+        static async Task<Mock<ILog>> RunStartableWarningTest(IWantToRunWhenEndpointStartsAndStops instanceToTest)
+        {
+            var thingsToBeStarted = new[] { instanceToTest };
+
+            var mockLogger = new Mock<ILog>();
+
+            var runner = new StartableAndStoppableRunner(thingsToBeStarted);
+
+            runner.LongRunningWarningTimeSpan = TimeToWarn;
+            StartableAndStoppableRunner.Log = mockLogger.Object;
+
+            await runner.Start(null);
+            await runner.Stop(null);
+
+            return mockLogger;
+        }
+
+        class LongRunningStartable : IWantToRunWhenEndpointStartsAndStops
+        {
+            public async Task Start(IMessageSession session)
+            {
+                await Task.Delay(LongRunningTimespan);
+            }
+
+            public async Task Stop(IMessageSession session)
+            {
+                await Task.Delay(LongRunningTimespan);
+            }
+        }
+
+        class QuickRunningStartable : IWantToRunWhenEndpointStartsAndStops
+        {
+            public async Task Start(IMessageSession session)
+            {
+                await Task.Delay(ShortRunningTimespan);
+            }
+
+            public async Task Stop(IMessageSession session)
+            {
+                await Task.Delay(ShortRunningTimespan);
+            }
+        }
+    }
+}
--- src/NServiceBus.Hosting.Windows/StartableAndStoppable/StartableAndStoppableRunner.cs
+++ src/NServiceBus.Hosting.Windows/StartableAndStoppable/StartableAndStoppableRunner.cs
@@ -21,9 +21,13 @@
             var startableTasks = new List<Task>();
             foreach (var startable in wantToRunWhenBusStartsAndStops)
             {
-                var task = startable.Start(session).ThrowIfNull();
-
                 var startable1 = startable;
+                var startableName = startable1.GetType().AssemblyQualifiedName;
+
+                var task = startable1.Start(session).ThrowIfNull();
+
+                LongRunningWarning(task,
+                    $"The start method for {startableName} is taking a long time to complete. The endpoint will not start until this operation has completed.");
 
                 /* 
                     We can't use the await keyword because of the conditional logging. 
@@ -32,13 +36,13 @@
                 task.ContinueWith(t =>
                 {
                     thingsRanAtStartup.Add(startable1);
-                    Log.DebugFormat("Started {0}.", startable1.GetType().AssemblyQualifiedName);
+                    Log.DebugFormat("Started {0}.", startableName);
                 }, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
                 task.ContinueWith(t =>
                 {
-                    Log.Error($"Startup task {startable1.GetType().AssemblyQualifiedName} failed to complete.", t.Exception);
+                    Log.Error($"Startup task {startableName} failed to complete.", t.Exception);
                 }, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
-                
+
                 startableTasks.Add(task);
             }
 
@@ -58,9 +62,13 @@
             {
                 try
                 {
+                    var stoppable1 = stoppable;
+                    var stoppableName = stoppable1.GetType().AssemblyQualifiedName;
+
                     var task = stoppable.Stop(session).ThrowIfNull();
 
-                    var stoppable1 = stoppable;
+                    LongRunningWarning(task,
+                        $"The stop method for {stoppableName} is taking a long time to complete. The endpoint will not shut down until this operation has completed.");
 
                     /* 
                         We can't use the await keyword because of the conditional logging. 
@@ -69,11 +77,11 @@
                     task.ContinueWith(t =>
                     {
                         thingsRanAtStartup.Add(stoppable1);
-                        Log.DebugFormat("Stopped {0}.", stoppable1.GetType().AssemblyQualifiedName);
+                        Log.DebugFormat("Stopped {0}.", stoppableName);
                     }, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously).Ignore();
                     task.ContinueWith(t =>
                     {
-                        Log.Fatal($"Startup task {stoppable1.GetType().AssemblyQualifiedName} failed to stop.", t.Exception);
+                        Log.Fatal($"Startup task {stoppableName} failed to stop.", t.Exception);
                         t?.Exception?.Flatten().Handle(e => true);
                     }, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously).Ignore();
 
@@ -96,8 +104,27 @@
             }
         }
 
+        void LongRunningWarning(Task taskToWatch, string message)
+        {
+            var delayTokenSource = new CancellationTokenSource();
+
+            var warningWaitTimeTask = Task.Delay(LongRunningWarningTimeSpan, delayTokenSource.Token);
+            Task.WhenAny(taskToWatch, warningWaitTimeTask)
+                .ContinueWith(t =>
+                {
+                    delayTokenSource.Cancel();
+                    delayTokenSource.Dispose();
+
+                    if (t.Result == warningWaitTimeTask)
+                    {
+                        Log.Warn(message);
+                    }
+                });
+        }
+
         IEnumerable<IWantToRunWhenEndpointStartsAndStops> wantToRunWhenBusStartsAndStops;
         ConcurrentBag<IWantToRunWhenEndpointStartsAndStops> thingsRanAtStartup = new ConcurrentBag<IWantToRunWhenEndpointStartsAndStops>();
-        static ILog Log = LogManager.GetLogger<StartableAndStoppableRunner>();
+        public static ILog Log = LogManager.GetLogger<StartableAndStoppableRunner>();
+        public TimeSpan LongRunningWarningTimeSpan = TimeSpan.FromMinutes(2);
     }
 }
