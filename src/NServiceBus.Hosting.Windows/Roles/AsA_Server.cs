357a1a68901d12b931e182f87ac4fcc9495e71cc
Sean Farmar <s.farmar@gmail.com>
2017-04-27 14:51:56 +0100

    Update AsA_Server.cs

--- src/NServiceBus.Hosting.Windows/Roles/AsA_Server.cs
+++ src/NServiceBus.Hosting.Windows/Roles/AsA_Server.cs
@@ -7,7 +7,7 @@ namespace NServiceBus
 
     [ObsoleteEx(
         TreatAsErrorFromVersion = "7.1",
-        Message = "Roles are obsoleted and should not be used. The functionality of AsA_Server, and AsA_Publisher has been made defaults in the core, see: https://docs.particular.net/nservicebus/hosting/nservicebus-host/?version=host_6#roles-built-in-configurations",
+        Message = "The functionality of AsA_Server, and AsA_Publisher has been made defaults in the core.",
         RemoveInVersion = "8.0")]
     public interface AsA_Server { }
 }
