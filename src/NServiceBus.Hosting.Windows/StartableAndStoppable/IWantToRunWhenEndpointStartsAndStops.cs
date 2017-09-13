9f9f0a38d89d60b497083a88c7b5225e9aeced19
andreasohlund <andreas.ohlund@particular.net>
2017-09-13 13:53:36 +0200

    Obsolete IWantToRunWhenEndpointStartsAndStops with WARN

--- src/NServiceBus.Hosting.Windows/StartableAndStoppable/IWantToRunWhenEndpointStartsAndStops.cs
+++ src/NServiceBus.Hosting.Windows/StartableAndStoppable/IWantToRunWhenEndpointStartsAndStops.cs
@@ -6,6 +6,8 @@
     /// Implementers will be invoked when the endpoint starts up.
     /// Dependency injection is provided for these types.
     /// </summary>
+    [ObsoleteEx(Message = "The windows host will be deprecated in the next major version. See upgrade guide and doco for alternatives.",
+        RemoveInVersion = "10.0")]
     public interface IWantToRunWhenEndpointStartsAndStops
     {
         /// <summary>
