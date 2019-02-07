c4cd8ba0fe680f3ef6154a81e40549965a59bd48
Brandon Ording <bording@gmail.com>
2019-02-07 14:32:12 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.Hosting.Tests/APIApprovals.cs
+++ src/NServiceBus.Hosting.Tests/APIApprovals.cs
@@ -1,17 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-using NServiceBus.Hosting.Tests;
-using NServiceBus.Hosting.Windows;
+﻿using NServiceBus.Hosting.Windows;
 using NUnit.Framework;
+using Particular.Approvals;
 using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
 {
     [Test]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public void Approve()
     {
         var publicApi = ApiGenerator.GeneratePublicApi(typeof(Program).Assembly);
-        TestApprover.Verify(publicApi);
+        Approver.Verify(publicApi);
     }
 }
